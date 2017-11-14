c41dee5b5cf176b4b8a33d82c91742d30f50df56
Adam Wargocki <a.wargocki@optimum.com.pl>
2017-11-14 09:42:49 +0100

    split data

--- publicapi.py
+++ publicapi.py
@@ -1,16 +1,24 @@
 import json
 import urllib.request
 
-url = 'https://bitbay.net/API/Public/BTCPLN/all.json'
+ticker_url = 'https://bitbay.net/API/Public/BTCPLN/ticker.json'
+market_url = 'https://bitbay.net/API/Public/BTCPLN/market.json'
+orderbook_url = 'https://bitbay.net/API/Public/BTCPLN/orderbook.json'
+trades_url = 'https://bitbay.net/API/Public/BTCPLN/trades.json'
 
 
 def download(url):
+    """download data from url"""
     return urllib.request.urlopen(url)
 
 
 def decode(data):
+    """decode json"""
     return json.loads(data.read().decode('utf-8'))
 
 
 if __name__ == '__main__':
-    print(decode(download(url)))
+    print(decode(download(ticker_url)))
+    print(decode(download(market_url)))
+    print(decode(download(orderbook_url)))
+    print(decode(download(trades_url)))
