72801911b081c4b618325a617ec0282f661961b9
Adam Wargocki <adam.wargocki@gmail.com>
2017-11-14 23:52:22 +0100

    remove min and max, change vlines

--- autobit.py
+++ autobit.py
@@ -18,13 +18,10 @@ ask_x,ask_y = zip(*asks)
 bid_y = list(itertools.accumulate(bid_y))
 ask_y = list(itertools.accumulate(ask_y))
 
-min_y = min(bid_y+ask_y)
-max_y = max(bid_y+ask_y)
-
 plt.plot(bid_x,bid_y,label='aids')
 plt.plot(ask_x,ask_y,label='asks')
 for key in 'max','min','last','vwap','average':
-    plt.plot((data_ticker[key],data_ticker[key]),(min_y,max_y),linestyle=':',label=key+'='+str(data_ticker[key]))
+    plt.axvline(data_ticker[key],linestyle=':',label=key+'='+str(data_ticker[key]))
 
 plt.legend()
 plt.show()
