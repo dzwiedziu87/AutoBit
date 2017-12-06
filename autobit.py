39a2d04e10743a9875e720236f870d9e884448bf
Adam Wargocki <a.wargocki@optimum.com.pl>
2017-12-06 13:51:48 +0100

    basic plot

--- autobit.py
+++ autobit.py
@@ -1,5 +1,14 @@
-from bitbay import BitBayApi
+from datetime import datetime
 
-api = BitBayApi("BTC",'1m')
+from bitbay import BitBayApi
+import matplotlib.pyplot as plt
+
+api = BitBayApi("BTC",'1h')
 api.update()
 print(api.data())
+
+data_x = [x['date'] for x in api.data()]
+data_y = [x['price'] for x in api.data()]
+
+plt.plot(data_x,data_y)
+plt.show()
