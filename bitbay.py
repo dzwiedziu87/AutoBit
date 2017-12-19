b3a131652e8930686b7ef3b19bda7ba2739a814a
Adam Wargocki <a.wargocki@optimum.com.pl>
2017-12-19 16:19:22 +0100

    bitbay public api

--- bitbay.py
+++ bitbay.py
@@ -1,6 +1,13 @@
 from download import download
 
-crypto_list = ['BTC','ETH','LSK','LTC','GAME','DASH','BCC','BTG']
-type_list = ['trades','orderbook','market','ticker','all']
+crypto_list = ['BTC', 'ETH', 'LSK', 'LTC', 'GAME', 'DASH', 'BCC', 'BTG']
+type_list = ['trades', 'orderbook', 'market', 'ticker', 'all']
 
 url = 'https://bitbay.net/API/Public/{crypto}PLN/{type}.json'
+
+
+def api(crypto, type, since=None):
+    current_url = url.format(crypto=crypto, type=type)
+    if since:
+        current_url = current_url + f'?since={since}'
+    return download(current_url)
