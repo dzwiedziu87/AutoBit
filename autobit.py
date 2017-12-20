a9e18fdee9b0f4c66d6a5316d49df7c0b9a11dfd
Adam Wargocki <a.wargocki@optimum.com.pl>
2017-12-20 08:57:46 +0100

    AppWindow class

--- autobit.py
+++ autobit.py
@@ -1,13 +1,20 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class AppWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.resize(300, 400)
+        self.setWindowTitle("AutoBit")
+        self.setWindowIcon(QIcon('icon.png'))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    w = QWidget()
-    w.resize(300,400)
-    w.setWindowTitle("AutoBit")
-    w.show()
+    autobit = AppWindow()
+    autobit.show()
 
     sys.exit(app.exec())
