110fee7c01acd5b898f6bb38b38ec21dddb2ebba
Adam Wargocki <a.wargocki@optimum.com.pl>
2017-12-20 13:57:44 +0100

    private api

--- download.py
+++ download.py
@@ -4,6 +4,6 @@ from urllib.request import urlopen
 
 def download(url):
     response = urlopen(url)
-    data = response.read().decode('utf-8')
+    data = response.read().decode()
     data_decoded = json.loads(data)
     return data_decoded
