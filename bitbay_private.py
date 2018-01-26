be7a9600c4f4f4635d72d534407206dcdb2f8259
Adam Wargocki <a.wargocki@optimum.com.pl>
2018-01-26 10:02:27 +0100

    change to private method

--- bitbay_private.py
+++ bitbay_private.py
@@ -5,10 +5,10 @@ from urllib.parse import urlencode
 
 from send import send_request
 
-url = 'https://bitbay.net/API/Trading/tradingApi.php'
+__url = 'https://bitbay.net/API/Trading/tradingApi.php'
 
 
-def send(data):
+def __send(data):
     key_pub = __read_from_file('key_pub')
     key_priv = __read_from_file('key_priv')
 
@@ -21,74 +21,60 @@ def send(data):
         'API-Hash': data_signed
     }
 
-    response_decoded = send_request(url, data_encoded, header)
+    response_decoded = send_request(__url, data_encoded, header)
 
     return response_decoded
 
 
 def info():
-    return send(
-        {
-            'method': 'info'
-        }
-    )
+    return __send({
+        'method': 'info'
+    })
 
 
 def trade(type, currency, payment_currency, amount, price):
-    return send(
-        {
-            'method': 'trade',
-            'type': type,
-            'currency': currency,
-            'payment_currency': payment_currency,
-            'amount': amount,
-            'rate': price
-        }
-    )
+    return __send({
+        'method': 'trade',
+        'type': type,
+        'currency': currency,
+        'payment_currency': payment_currency,
+        'amount': amount,
+        'rate': price
+    })
 
 
 def cancel(id):
-    return send(
-        {
-            'method': 'cancel',
-            'id': id
-        }
-    )
+    return __send({
+        'method': 'cancel',
+        'id': id
+    })
 
 
 def orderbook(order_currency, payment_currency):
-    return send(
-        {
-            'method': 'orderbook',
-            'order_currency': order_currency,
-            'payment_currency': payment_currency
-        }
-    )
+    return __send({
+        'method': 'orderbook',
+        'order_currency': order_currency,
+        'payment_currency': payment_currency
+    })
 
 
 def orders():
-    return send(
-        {
-            'method': 'orders'
-        }
-    )
+    return __send({
+        'method': 'orders'
+    })
 
 
 def history(currency):
-    return send(
-        {
-            'method': 'history',
-            'currency': currency
-        }
-    )
+    return __send({
+        'method': 'history',
+        'currency': currency
+    })
 
 
 def transactions():
-    return send(
-        {
-            'method': 'transactions'
-        }
-    )
+    return __send({
+        'method': 'transactions'
+    })
 
 
 def __read_from_file(file_name):
